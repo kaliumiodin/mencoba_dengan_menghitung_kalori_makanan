04340ea2a9ed6e7db2a81ca6569cd2da4ae13269
kaliumiodin <kailakf07@gmail.com>
2025-12-11 15:25:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.write("Hello, *World!* :sunglasses:")
 
 st.title("MY WEBSITE NEW 2025")
-st.title("_Kaila_ is :blue[cool] :sunglasses:")
+st.title("_Kaila_ is :blue[keren] :sunglasses:")
