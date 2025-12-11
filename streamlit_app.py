d9fe61fa166022fa79efc518d66ae4cf985b2854
kaliumiodin <kailakf07@gmail.com>
2025-12-11 15:32:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,2 @@
-import streamlit as st
-st.write("Hello, *World!* :sunglasses:")
-
-st.title(":red[MY WEBSITE NEW 2025]")
-st.title(":red[_Kaila_ ]is :blue[keren] :sunglasses:")
-import streamlit as st
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.header("A pizza")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A noodle")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An macaroon")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+st.title(":red[Percobaan Hari Ini]")
+st.title(":red[Penentuan Bilangan Ganjil dan Genap]")
