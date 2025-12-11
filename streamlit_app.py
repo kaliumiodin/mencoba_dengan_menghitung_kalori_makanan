f07ca8352bd571f51ad31a58110b2cd1d2080b8c
kaliumiodin <kailakf07@gmail.com>
2025-12-11 15:45:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title(":red[Percobaan Hari Ini]")
 st.header(":blue[Penentuan Bilangan Ganjil dan Genap]")
 number =st.number_input("Insert a number",min_value=0,max_value=10000)
-if number%2==1
+if number%2==1:
   st.write("Bilangan",number,"Termasuk bilangan ganjil")
 else:
   st.write("Bilangan",number,"Termasuk bilangan genap")
